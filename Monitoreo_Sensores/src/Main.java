03603b7e46a7649ff7f72ea19637d6dd5065a176
sebastian trillos <trillossebastian148@gmail.com>
2026-02-20 09:43:29 -0500

    se termina el modulo Monitoreo_Sensores

--- Monitoreo_Sensores/src/Main.java
+++ Monitoreo_Sensores/src/Main.java
@@ -15,8 +15,9 @@ public class Main {
             }
         }
 
-    Main.promedioZonas ( temperaturas );
-
+        Main.promedioZonas ( temperaturas );
+        System.out.println ("El promedio general es de : "+ Main.promedioGeneral ( temperaturas ));
+        Main.valoresCriticos ( temperaturas );
     }
 
     public static void promedioZonas(double[][] temperaturas){
@@ -28,6 +29,30 @@ public class Main {
             }
             System.out.println ("el promedio de la zona "+(i+1)+ " es de : " + (promedioZona/ temperaturas.length));
         }
+    }
 
+    public static double promedioGeneral(double[][] temperaturas){
+        double suma = 0;
+        int contador = 0;
+        for (int i = 0; i < temperaturas.length; i++) {
+            for (int j = 0; j < temperaturas[i].length; j++) {
+                suma+=temperaturas[i][j];
+                contador++;
+            }
+        }
+        return suma/ contador;
+    }
+
+    public static void valoresCriticos(double[][] temperaturas){
+        System.out.println ("temperaturas criticas ");
+        for (int i = 0; i < temperaturas.length; i++) {
+            for (int j = 0; j < temperaturas[i].length; j++) {
+                if (temperaturas[i][j] >99) {
+                    System.out.println ("temperatura : "+ temperaturas[i][j]);
+                }else if (temperaturas[i][j]<0){
+                    System.out.println ("temperatura : "+ temperaturas[i][j]);
+                }
+            }
+        }
     }
 }
