5426406170b12652c3418331727e5efe0590b9ab
sebastian <trillossebastian128@gmail.com>
2026-02-27 07:33:05 -0500

    se termina ListaContactos

--- listas_simples/sistema_contacto/src/Nodo.java
+++ listas_simples/sistema_contacto/src/Nodo.java
@@ -1,8 +1,24 @@
 public class Nodo {
     private Contacto dato;
-    Nodo siguiente;
+    private Nodo siguiente;
 
     public Nodo(Contacto dato){
+        this.setDato(dato);
+    }
+
+    public Contacto getDato() {
+        return dato;
+    }
+
+    public void setDato(Contacto dato) {
         this.dato = dato;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
     }
 }
