aa94b6865390d4cff0d4623900878842d3c567cc
sebastian <trillossebastian148@gmail.com>
2026-03-06 07:46:45 -0500

    new commit

--- listas_simples/historial_navegacion/src/Main.java
+++ listas_simples/historial_navegacion/src/Main.java
@@ -6,5 +6,20 @@ public class Main {
         lista.insertarfinal(new Pagina("YouTube", "https://www.youtube.com"));
         lista.insertarfinal(new Pagina("Wikipedia", "https://www.wikipedia.org"));
 
+
+        System.out.println("lista navegacion actual \n");
+
+        lista.listar();
+
+        System.out.println("\n buscar google : ");
+        System.out.println(lista.buscar("Google"));
+
+        System.out.println("\n eliminar a wikipedia : ");
+        System.out.println(lista.eliminar("Wikipedia"));
+
+
+        System.out.println("\n Lista navegacion actual : ");
+
+        lista.listar();
     }
 }
--- listas_simples/historial_navegacion/src/Pagina.java
+++ listas_simples/historial_navegacion/src/Pagina.java
@@ -4,14 +4,18 @@ import java.net.MalformedURLException;
 
 public class Pagina {
     private String nombre;
-    private URL url;
+    private String url;
     private LocalDateTime fechaHora;
 
-    public Pagina(String nombre, String urlTexto)  throws MalformedURLException{
+    public Pagina(String nombre, String urlTexto){
         this.setNombre(nombre);
-        this.url = new URL(urlTexto);
+        this.url = urlTexto;
         this.fechaHora = LocalDateTime.now();
     }
+
+    public String getUrl() { return url;}
+
+    public void setUrl(String url) { this.url = url;}
 
     public String getNombre() {
         return nombre;
@@ -21,19 +25,20 @@ public class Pagina {
         this.nombre = nombre;
     }
 
-    public URL getUrl() {
-        return url;
-    }
-
-    public void setUrl(URL url) {
-        this.url = url;
-    }
-
     public LocalDateTime getFechaHora() {
         return fechaHora;
     }
 
     public void setFechaHora(LocalDateTime fechaHora) {
         this.fechaHora = fechaHora;
+    }
+
+    @Override
+    public String toString() {
+        return "Pagina{" +
+                "nombre='" + nombre + '\'' +
+                ", url='" + url + '\'' +
+                ", fechaHora=" + fechaHora +
+                '}';
     }
 }
