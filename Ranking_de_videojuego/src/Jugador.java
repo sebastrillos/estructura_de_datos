e028123cad61a5c8148051a51a8f35f20b1e63b3
pepito sebastian <trillossebastian148@gmail.com>
2026-03-10 07:52:24 -0500

    se crea ranking videojuego

---
+++ Ranking_de_videojuego/src/Jugador.java
@@ -0,0 +1,35 @@
+public class Jugador {
+    private String nombre;
+    private String tipo;
+    private int ranking;
+
+    public Jugador(String nombre, String tipo, int ranking) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.ranking = ranking;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(int ranking) {
+        this.ranking = ranking;
+    }
+}
---
+++ Ranking_de_videojuego/src/ListaRanking.java
@@ -0,0 +1,3 @@
+public class ListaRanking {
+    private Nodo cabeza;
+}
